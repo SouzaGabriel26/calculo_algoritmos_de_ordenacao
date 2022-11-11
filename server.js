b2cfdce2b456a47c1801a852bfbbb3b33dd90521
Gabriel Alves <gabriel.asouza2608@hotmail.com>
2022-11-11 16:54:07 -0300

    express server

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express")
+const app = express()
+const path = require("path")
+const port = 3000
+
+app.use(express.static(path.join(__dirname)))
+
+app.use("/calcs", express.static(path.join(__dirname + "/public/calculo.html")))
+
+app.listen(port, () => {
+  console.log("server running on port 3000")
+})
